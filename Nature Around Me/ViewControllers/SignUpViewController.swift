cab489045a64f454c894f6f509bca6d0639c57ff
Ron David <77014885+Ron-David@users.noreply.github.com>
2021-06-30 12:01:56 +0300

    img

--- Nature Around Me/ViewControllers/SignUpViewController.swift
+++ Nature Around Me/ViewControllers/SignUpViewController.swift
@@ -7,36 +7,45 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var avatar: UIImageView!
     
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var name: UITextField!
-    
+    var image: UIImage?
     
     @IBAction func addAvatar(_ sender: Any) {
-//        if UIImagePickerController.isSourceTypeAvailable(
-//            UIImagePickerController.SourceType.camera) {
-//            let imagePicker = UIImagePickerController()
-//            imagePicker.delegate = self
-//            imagePicker.sourceType = UIImagePickerController.SourceType.camera;
-//            imagePicker.allowsEditing = true
-//            self.present(imagePicker, animated: true, completion: nil)
-//        }else{
-//            let imagePicker = UIImagePickerController()
-//            imagePicker.delegate = self
-//            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary;
-//            imagePicker.allowsEditing = true
-//            self.present(imagePicker, animated: true, completion: nil)
-//        }
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }else{
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        self.avatar.image = image
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func signUp(_ sender: Any) {
-        if(email.text == "" || password.text == "" || name.text == "" ){
+        if(email.text == "" || password.text == "" || name.text == "" || password.text!.count<6 || password.text!.count>12){
             Alert.alertGeneral(on: self, with: "Opss...", message: "Please make sure all fields are filled in correctly.")
             
+            return
+        }
+        if(!isValidEmail(email.text!)){
+            Alert.alertGeneral(on: self, with: "Opss...", message: "Please make sure your email address is correct.")
             return
         }
 
@@ -52,7 +61,12 @@ class SignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
     
     /*
     // MARK: - Navigation
