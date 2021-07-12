0ff24abc5d8dedbd1ab01651ea279e1d1f0c2574
Ron David <77014885+Ron-David@users.noreply.github.com>
2021-07-12 21:25:59 +0300

    page for creating posts

--- Nature Around Me/Model/ImgPicker.swift
+++ Nature Around Me/Model/ImgPicker.swift
@@ -31,4 +31,6 @@ struct ImgPicker {
     }
     
     
+    
+    
 }
--- Nature Around Me/ViewControllers/CreateViewController.swift
+++ Nature Around Me/ViewControllers/CreateViewController.swift
@@ -7,18 +7,70 @@
 
 import UIKit
 
-class CreateViewController: UIViewController {
+class CreateViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
     @IBOutlet weak var location: UITextField!
 
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var detailsText: UITextView!
     @IBOutlet weak var postTitle: UITextField!
         
-    
+    let defaultImage = UIImage(systemName: "person.fill")
+    @IBOutlet weak var img1: UIImageView!
+    @IBOutlet weak var img2: UIImageView!
+    @IBOutlet weak var img3: UIImageView!
+    var imgIndex:Int!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.spinner.stopAnimating()
         // Do any additional setup after loading the view.
     }
+    @IBAction func clearButton(_ sender: Any) {
+        img1.image = defaultImage
+        img2.image = defaultImage
+        img3.image = defaultImage
+        detailsText.text = ""
+        postTitle.text = ""
+        location.text = ""
+    }
+    @IBAction func addImg1(_ sender: Any) {
+        imgIndex = 1
+        ImgPicker.pickImg(on: self);
+    }
+    
+    @IBAction func addImg2(_ sender: Any) {
+        imgIndex = 2
+        ImgPicker.pickImg(on: self);
+    }
+    
+    @IBAction func addImg3(_ sender: Any) {
+        imgIndex = 3
+        ImgPicker.pickImg(on: self);
+    }
+    //Catching the img
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.spinner.startAnimating()
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        switch imgIndex {
+        case 1:
+            self.img1.image = image
+        case 2:
+            self.img2.image = image
+        case 3:
+            self.img3.image = image
+        default:
+            self.img1.image = image
+        }
+//        Model.instance.saveImage(image: image!) { url in
+//            self.spinner.stopAnimating()
+//
+//        }
+        self.spinner.stopAnimating()
+
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func createButton(_ sender: Any) {
         let randId = generateRandomId()
         let post = Post.create(id: randId, title: postTitle.text!, location: location.text!,imageUrl: "", isActive: true)
@@ -27,6 +79,7 @@ class CreateViewController: UIViewController {
         }
         navigationController?.popViewController(animated: true)
     }
+    
     
     func generateRandomId() -> String {
       let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
